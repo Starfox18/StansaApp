ae852418f96105f38f59a737dabbbb9497504274
starfox18 <italo.amez>
2015-06-25 17:32:01 -0500

    Cambios en el Attention

--- StansaController/AttentionDB.cpp
+++ StansaController/AttentionDB.cpp
@@ -18,17 +18,17 @@ void AttentionDB::Add(Attention^ a){
 	//Paso 2: Preparamos la sentencia
 	SqlCommand^ comm = gcnew SqlCommand();
 	comm->Connection = conn;
-	comm->CommandText = "INSERT INTO Attention_DB " +
+	comm->CommandText = "INSERT INTO AttentionDB " +
 		" (fecha, n_orden, hora_ini, hora_fin, estado, customerId, moduloStansaId, staffId)" +
 		" VALUES (@p1,@p2,@p3,@p4,@p5,@p6,@p7,@p8)";
 	SqlParameter^ p1 = gcnew SqlParameter("@p1",
-		System::Data::SqlDbType::Date);
+		System::Data::SqlDbType::VarChar);
 	SqlParameter^ p2 = gcnew SqlParameter("@p2",
 		System::Data::SqlDbType::Int);
 	SqlParameter^ p3 = gcnew SqlParameter("@p3",
-		System::Data::SqlDbType::Time);
+		System::Data::SqlDbType::VarChar);
 	SqlParameter^ p4 = gcnew SqlParameter("@p4",
-		System::Data::SqlDbType::Time);
+		System::Data::SqlDbType::VarChar);
 	SqlParameter^ p5 = gcnew SqlParameter("@p5",
 		System::Data::SqlDbType::VarChar);
 	SqlParameter^ p6 = gcnew SqlParameter("@p6",
@@ -70,17 +70,17 @@ void AttentionDB::Update(Attention^ a){
 	//Paso 2: Preparamos la sentencia
 	SqlCommand^ comm = gcnew SqlCommand();
 	comm->Connection = conn;
-	comm->CommandText = "UPDATE Attention_DB " +
+	comm->CommandText = "UPDATE AttentionDB " +
 		"SET fecha=@p1, n_orden=@p2, hora_ini=@p3, hora_fin=@p4, estado=@p5, customerId=@p6 , moduloStansaId=@p7 , staffId=@p8  " +
 		" WHERE id=@p9";
 	SqlParameter^ p1 = gcnew SqlParameter("@p1",
-		System::Data::SqlDbType::Date);
+		System::Data::SqlDbType::VarChar);
 	SqlParameter^ p2 = gcnew SqlParameter("@p2",
 		System::Data::SqlDbType::Int);
 	SqlParameter^ p3 = gcnew SqlParameter("@p3",
-		System::Data::SqlDbType::Time);
+		System::Data::SqlDbType::VarChar);
 	SqlParameter^ p4 = gcnew SqlParameter("@p4",
-		System::Data::SqlDbType::Time);
+		System::Data::SqlDbType::VarChar);
 	SqlParameter^ p5 = gcnew SqlParameter("@p5",
 		System::Data::SqlDbType::VarChar);
 	SqlParameter^ p6 = gcnew SqlParameter("@p6",
@@ -158,6 +158,8 @@ Attention^ AttentionDB::QueryById(int id){
 	comm->Parameters->Add(p1);
 	//Paso 3: Ejecución de la sentencia
 	SqlDataReader^ dr = comm->ExecuteReader();
+
+
 	//Paso 3.1: Procesamos los resultados	
 	Attention ^ a = nullptr;
 	if (dr->Read()){
@@ -190,7 +192,7 @@ List<Attention^>^ AttentionDB::QueryAll(){
 	SqlConnection^ conn;
 	conn = gcnew SqlConnection();
 	conn->ConnectionString = "Server=inti.lab.inf.pucp.edu.pe;" +
-		"Database=inf237;User ID=inf237g4;Password=wXJ7FpUHDnYKjf89;";
+		"Database=inf237g4;User ID=inf237g4;Password=wXJ7FpUHDnYKjf89;";
 	conn->Open();
 	//Paso 2: Preparamos la sentencia
 	SqlCommand^ comm = gcnew SqlCommand();
@@ -204,22 +206,22 @@ List<Attention^>^ AttentionDB::QueryAll(){
 	while (dr->Read()){
 		Attention^a = gcnew Attention();
 		a->id = (int)dr["id"];
-		if (dr["fecha"] != System::DBNull::Value)
-			a->fecha = safe_cast<DateTime^>(dr["fecha"]);
-		if (dr["n_orden"] != System::DBNull::Value)
-			a->n_orden = safe_cast<int>(dr["n_orden"]);
+		if (dr["date"] != System::DBNull::Value)
+			a->fecha = safe_cast<DateTime^>(dr["date"]);
+		if (dr["orderNumber"] != System::DBNull::Value)
+			a->n_orden = safe_cast<int>(dr["orderNumber"]);
 		if (dr["inTime"] != System::DBNull::Value)
-			a->hora_ini = safe_cast<DateTime^>(dr["hora_ini"]);
-		if (dr["hora_fin"] != System::DBNull::Value)
-			a->hora_fin = safe_cast<DateTime^>(dr["hora_fin"]);
-		if (dr["estado"] != System::DBNull::Value)
-			a->estado = safe_cast<String^>(dr["estado"]);
-		if (dr["customerId"] != System::DBNull::Value)
-			a->customer->id = safe_cast<int>(dr["customerId"]);
-		if (dr["moduloStansaId"] != System::DBNull::Value)
-			a->moduloStansa->id = safe_cast<int>(dr["moduloStansaId"]);
-		if (dr["staffId"] != System::DBNull::Value)
-			a->staff->id = safe_cast<int>(dr["staffId"]);
+			a->hora_ini = safe_cast<DateTime^>(dr["inTime"]);
+		if (dr["outTime"] != System::DBNull::Value)
+			a->hora_fin = safe_cast<DateTime^>(dr["outTime"]);
+		if (dr["status"] != System::DBNull::Value)
+			a->estado = safe_cast<String^>(dr["status"]);
+		if (dr["idCustomer"] != System::DBNull::Value)
+			a->customer->id = safe_cast<int>(dr["idCustomer"]);
+		if (dr["idModStansa"] != System::DBNull::Value)
+			a->moduloStansa->id = safe_cast<int>(dr["idModStansa"]);
+		if (dr["idStaff"] != System::DBNull::Value)
+			a->staff->id = safe_cast<int>(dr["idStaff"]);
 
 		AttentionList->Add(a);
 	}
@@ -228,6 +230,8 @@ List<Attention^>^ AttentionDB::QueryAll(){
 	conn->Close();
 	return AttentionList;
 }
+
+
 List<Attention^>^ AttentionDB::QueryAllByModuloStansa(ModuloStansa^ modulo){ 
 	SqlConnection^ conn;
 	conn = gcnew SqlConnection();
