3888c7b4add1759cab17bda77e1e01118446c73c
johnpozo7 <a20111120@pucp.pe>
2015-06-25 21:45:16 -0500

    casifinal2

--- UserApp/StatusForm.h
+++ UserApp/StatusForm.h
@@ -17,6 +17,9 @@ namespace UserApp {
 	/// </summary>
 	public ref class StatusForm : public System::Windows::Forms::Form
 	{
+
+	private:
+		Thread ^myThread;
 	public:
 		StatusForm(void)
 		{
@@ -24,7 +27,21 @@ namespace UserApp {
 			//
 			//TODO: agregar código de constructor aquí
 			//
+			myThread = gcnew Thread(gcnew ThreadStart(this, &StatusForm::MyRun));
+			myThread->Start();
 		}
+		void MyRun(){
+			int time = 0;
+			while (true) {
+				Invoke(gcnew delegateRefreshProducts(this, &StatusForm::RefreshDGVStansas));
+				Random^ rnd = gcnew Random();
+				time = rnd->Next() % 10 + 1;
+				Thread::Sleep(time * 1000);
+				if (!this->Visible)
+					return;
+			}
+		}
+		delegate void delegateRefreshProducts();
 
 	protected:
 		/// <summary>
